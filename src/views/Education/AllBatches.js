e5ed0d60faebb34f2dc28ed178633464bf99b40d
Ankit <kabra4215@gmail.com>
2023-08-04 11:51:49 +0530

    today 04 changes

--- src/views/Education/AllBatches.js
+++ src/views/Education/AllBatches.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { cilPlus, cilTrash, cilOptions } from '@coreui/icons'
+import { CMultiSelect } from '@coreui/react-pro'
 import {
   CSmartTable,
   CButton,
@@ -23,12 +24,17 @@ import CIcon from '@coreui/icons-react'
 import { cilPen, cilArrowLeft } from '@coreui/icons'
 import AuthFun from 'src/components/Pages/AuthFunction/AuthFun'
 import { CCol as Col } from '@coreui/react'
+import { CInputGroup, CInputGroupText, CFormSelect } from '@coreui/react'
 
 function AllBatches() {
   document.title = 'Eclass - All Batch'
   const [details, setDetails] = useState([])
   const [visibleDelete, setVisibleDelete] = useState(false)
   const [visibleEdit, setVisibleEdit] = useState(false)
+
+  const [slugSet, setSlugSet] = useState(' ')
+  const [dataSetup, setDataSetup] = useState([])
+
   const Cimg = 'https://cdn.pixabay.com/photo/2023/05/27/18/15/barn-swallows-8022044_1280.jpg'
   const columns = [
     {
@@ -141,20 +147,170 @@ function AllBatches() {
     console.log('Working Done')
   }
 
+  const options = [
+    {
+      value: 0,
+      text: 'Angular',
+    },
+    {
+      value: 1,
+      text: 'Bootstrap',
+    },
+    {
+      value: 2,
+      text: 'React.js',
+    },
+    {
+      value: 3,
+      text: 'Vue.js',
+    },
+    {
+      label: 'backend',
+      options: [
+        {
+          value: 4,
+          text: 'Django',
+        },
+        {
+          value: 5,
+          text: 'Laravel',
+        },
+        {
+          value: 6,
+          text: 'Node.js',
+        },
+      ],
+    },
+  ]
+
   return (
     <>
       <div>
         <AuthFun />
         <CRow>
           <CCol xs={4}>
-            <div className="background-white-border-radious padding-20px-10px mb-4"></div>
+            <div className="background-white-border-radious padding-20px-10px mb-4">
+              <div>
+                <p className="text-weight-1-3rem">Add Batch</p>
+              </div>
+              <hr />
+              <CForm>
+                <div className="width-dec10">
+                  <CFormInput
+                    type="text"
+                    id="exampleFormControlInput2"
+                    label="Batch Name :"
+                    placeholder="Please Enter Assignments Name"
+                    onChange={(e) => {
+                      let dataInput = e.target.value
+                      setDataSetup((value) => ({ ...value, categoryName: dataInput }))
+                      let smallCase = dataInput.toLocaleLowerCase().trim()
+                      let removeSpace = smallCase.replaceAll(' ', '-')
+                      setSlugSet(removeSpace)
+                    }}
+                    text="The name is how it appears on your Site"
+                    aria-describedby="exampleFormControlInputHelpInline"
+                  />
+                </div>
+
+                <div className="width-dec10 margin-down-and-top">
+                  <CFormInput
+                    type="text"
+                    id="exampleFormControlInput2"
+                    value={slugSet}
+                    // onChange={(e) => {
+                    //   let slugDataSet = e.target.value
+                    //   setDataSetup((value) => ({ ...value, slug: slugDataSet }))
+                    //   setSlugSet(setDataSetup.slug)
+                    // }}
+                    label="Slug :"
+                    placeholder="Please-enter-slug"
+                    text='The "slug" is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.'
+                    aria-describedby="exampleFormControlInputHelpInline"
+                  />
+                </div>
+
+                <div className="margin-down-and-top">
+                  <CFormLabel>Image :</CFormLabel>
+                  <div className="d-flex justify-content-space-evenly margin-down-and-top">
+                    <CFormInput
+                      // onChange={(e) => {
+                      //   let Image = e.target.files[0]
+                      //   setDataSetup((value) => ({ ...value, image: URL.createObjectURL(Image) }))
+                      // }}
+                      className="width-dec10"
+                      type="file"
+                      size="fit"
+                      id="formFileLg"
+                    />
+                  </div>
+                </div>
+
+                <div className="margin-down-and-top">
+                  <CFormLabel>Select Course: *</CFormLabel>
+                  <CInputGroup className="mb-3">
+                    <CFormSelect id="inputGroupSelect02">
+                      <option>Choose...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </CFormSelect>
+                  </CInputGroup>
+                </div>
+
+                <div>
+                  <CMultiSelect
+                    options={options}
+                    label="Select Users: *"
+                    text="Please select your User"
+                  />
+                </div>
+
+                <div className="margin-down-and-top">
+                  <div className="d-flex margin-down-and-top">
+                    <div className="margin-right-40px">
+                      <CFormLabel>Featured :</CFormLabel>
+                      <CFormSwitch
+                        // onChange={(e) => {
+                        //   if (featuredStateManage) {
+                        //     setFeaturedStateManage('false')
+                        //     setDataSetup((value) => ({ ...value, isFeatured: featuredStateManage }))
+                        //   }
+                        //   if (featuredStateManage === 'false') {
+                        //     setFeaturedStateManage('true')
+                        //     setDataSetup((value) => ({ ...value, isFeatured: featuredStateManage }))
+                        //   }
+                        // }}
+                        id="formSwitchCheckChecked"
+                      />
+                    </div>
+                    <div>
+                      <CFormLabel>Status :</CFormLabel>
+                      <CFormSwitch
+                        // onChange={(e) => {
+                        //   // if (statusStateManage) {
+                        //   //   setStatusStateManage('false')
+                        //   //   setDataSetup((value) => ({ ...value, isActive: statusStateManage }))
+                        //   // }
+                        //   // if (statusStateManage === 'false') {
+                        //   //   setStatusStateManage('true')
+                        //   //   setDataSetup((value) => ({ ...value, isActive: statusStateManage }))
+                        //   // }
+                        // }}
+                        id="formSwitchCheckChecked"
+                      />
+                    </div>
+                  </div>
+                  <div className="margin-down-and-top">
+                    <CButton color="primary">Create</CButton>
+                  </div>
+                </div>
+              </CForm>
+            </div>
           </CCol>
           <Col className="background-white-border-radious">
             <div className="display-flex-justify-space-between-padding">
               <div>
-                <CButton className="mx-3" color="success" variant="outline">
-                  <CIcon icon={cilPlus}></CIcon> Add Batch
-                </CButton>
                 <CButton className="mx-3" color="warning" variant="outline">
                   <CIcon icon={cilTrash}></CIcon> Delete Selected
                 </CButton>
